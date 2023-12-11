98ba938223ca156c8ae8a8013199616c25f1301b
Luis Pinos <149178337+LAPinosB@users.noreply.github.com>
2023-12-11 19:43:25 +0100

    version 1.0
    Creacion de JPA con Hibernate, a diferencia del video que lo hace con eclipse. Viene a ser lo mismo simplemente que yo podre trabajar ya con hibernate

---
+++ src/main/java/com/mycompany/jpapruebaluis/JpaPrueba.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.jpapruebaluis;
+
+/**
+ *
+ * @author Luis
+ */
+public class JpaPrueba {
+
+    /**
+     * @param args the command line arguments
+     * https://projectlombok.org/features/GetterSetter
+     * Existe lombok para realizar @Getter y @Setter esto lo hice averiguando e investigando hibernate
+     * en los atributos de las clases que necesite get and set
+     */
+   public static void main(String[] args) {
+       
+    }
+    
+}
